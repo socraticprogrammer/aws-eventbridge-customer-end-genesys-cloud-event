bc38ae06c7986cc3786b51ad635bd3a6a59ab033
Thalles <thalles.lossurdo@invenit.com.br>
2023-10-06 12:00:13 -0300

    chore: add variables for genesys cloud event bridge integration

---
+++ iac/modules/genesys_cloud_event_bridge_integration/variables.tf
@@ -0,0 +1,19 @@
+variable "integration_name" {
+  type    = string
+}
+
+variable "topic_filters" {
+  type = list(string)
+}
+
+variable "event_source_suffix" {
+  type = string
+}
+
+variable "aws_account_region" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
